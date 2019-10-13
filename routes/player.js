eff5aab282d0489411e3886807e3e7860b5b9242
dforce2055 <dperez2055@gmail.com>
2019-10-13 16:21:20 -0300

    Agrego modificaciones en api/player

--- routes/player.js
+++ routes/player.js
@@ -2,63 +2,61 @@ const { verificaToken, verificaAdminRol } = require('../middlewares/auth');
 const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
+const colors = require('colors');
 // Conexión a mysql
-let mysql = require('mysql');
+const mysql = require('mysql');
 
 // Conexión a mysql compatible con HEROKU
-let mysqlConnection = mysql.createConnection(process.env.MYSQL_URL);
-
-const app = express();
+const mysqlConnection = mysql.createConnection(process.env.MYSQL_URL);
+mysqlConnection.connect(function (err) {
+    if (err) {
+        console.error('MySQL error: ' + err.message);
+        return;
+    }
+    console.log(`Conectado a MySQL Id:- ${mysqlConnection.threadId}` .green );
+});
 const collection = 'players'; 
 
+const app = express();
+
+
 app.get('/api/players', /*[verificaToken],*/ (req, res) => {
-    mysqlConnection.connect(function (err) {
-        if (err) {
-            //throw err;
-            console.log("No pude conectarme a MySQL".red);
-        }
-        //console.log("Conectado a MySQL!".red);
+    const sql =`
+                select *
+                from ${ collection }
+            `;
 
-        const sql =
-            `select *
-                from ${ collection }`;
+    mysqlConnection.query(sql, function (err, players, fields) {
+        if (err) throw err;
 
-        mysqlConnection.query(sql, function (err, players, fields) {
-            //mysqlConnection.end();
-            if (err) throw err;
+        console.log(`Consultando players`);
+        const jugadores = JSON.parse(JSON.stringify(players));
 
-            console.log(`Consultando players`);
-            const productos = JSON.parse(JSON.stringify(players));
-            //console.log(productos);
-            res.json(productos)
-        });
+        res.json(jugadores)
     });
 });
 
 app.post('/api/player', /*[verificaToken],*/(req, res) => {
-    mysqlConnection.connect(function (err) {
-        if (err) {
-            //throw err;
-            console.log("No pude conectarme a MySQL".red);
-        }
+    let body = req.body;
+    console.log("Insertando un player...");
 
-        let body = req.body;
-        console.log("Insertando un player...");
+    const sql = `
+                INSERT INTO ${collection}(name, \`middle-name\`, \`last-name\`, age, email, state, \`role\`,\`level\`)
+                VALUES('${body.name}', '${body.middlename}', '${body.lastname}' , '${body.age}', '${body.email}', 1, '${body.role}', '${body.level}')
+            `;
 
+    mysqlConnection.query(sql, function (err, result) {
+        //mysqlConnection.end();
+        if (err) throw err;
 
-        const sql = `
-                    INSERT INTO ${collection}(name, \`middle-name\`, \`last-name\`, age, email, state, \`role\`,\`level\`)
-                    VALUES('${body.name}', '${body.middlename}', '${body.lastname}' , '${body.age}', '${body.email}', 1, '${body.role}', '${body.level}')
-                    `;
-
-        mysqlConnection.query(sql, function (err, result) {
-            //mysqlConnection.end();
-            if (err) throw err;
-
-            console.log("1 player inserted, ID: " + result.insertId);
-        });
+        console.log("1 player inserted, ID: " + result.insertId);
+        res.json({
+            ok: true,
+            id_nuevo_jugador: result.insertId
+        })
     });
 });
+
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
