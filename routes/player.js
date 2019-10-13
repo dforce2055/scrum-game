5c705abfd1e5d19a0e870d9c8bdeb215e94c1e27
dforce2055 <dperez2055@gmail.com>
2019-10-13 15:59:14 -0300

    Agrego metodo para aadir un jugador a traves de la API

--- routes/player.js
+++ routes/player.js
@@ -24,13 +24,38 @@ app.get('/api/players', /*[verificaToken],*/ (req, res) => {
                 from ${ collection }`;
 
         mysqlConnection.query(sql, function (err, players, fields) {
-            mysqlConnection.end();
+            //mysqlConnection.end();
             if (err) throw err;
 
             console.log(`Consultando players`);
             const productos = JSON.parse(JSON.stringify(players));
             //console.log(productos);
             res.json(productos)
+        });
+    });
+});
+
+app.post('/api/player', /*[verificaToken],*/(req, res) => {
+    mysqlConnection.connect(function (err) {
+        if (err) {
+            //throw err;
+            console.log("No pude conectarme a MySQL".red);
+        }
+
+        let body = req.body;
+        console.log("Insertando un player...");
+
+
+        const sql = `
+                    INSERT INTO ${collection}(name, \`middle-name\`, \`last-name\`, age, email, state, \`role\`,\`level\`)
+                    VALUES('${body.name}', '${body.middlename}', '${body.lastname}' , '${body.age}', '${body.email}', 1, '${body.role}', '${body.level}')
+                    `;
+
+        mysqlConnection.query(sql, function (err, result) {
+            //mysqlConnection.end();
+            if (err) throw err;
+
+            console.log("1 player inserted, ID: " + result.insertId);
         });
     });
 });
